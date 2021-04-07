614c6d57b454a46bca5c7d6c6832f351ecb4e06f
rsthakur83 <rsthakur83@yahoo.com>
2021-04-07 17:46:26 +0200

    Update lc-asg.tf

--- app/deploy/lc-asg.tf
+++ app/deploy/lc-asg.tf
@@ -3,7 +3,7 @@ resource "aws_launch_configuration" "APP-LC" {
   name                 = "APP-LC"
   depends_on           = ["aws_iam_role_policy_attachment.cw_db_policy_attach"] // "aws_security_group.APP-SG-Web"]  addressed required */
   image_id             = var.image_id
-  instance_type        = "t2.micro"
+  instance_type        = var.instance-type
   iam_instance_profile = "cwdb_iam_profile"
   security_groups      = [aws_security_group.app_asg.id]
   user_data            = file("/root/project/app/deploy/userdata-asg.sh")
