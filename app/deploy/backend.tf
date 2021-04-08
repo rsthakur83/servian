57a3553de7f5195280a5157ae3354d3df7f55a7c
rsthakur83 <rsthakur83@yahoo.com>
2021-04-08 09:45:44 +0200

    Update backend.tf

--- app/deploy/backend.tf
+++ app/deploy/backend.tf
@@ -1,7 +1,7 @@
 # Terraform S3 backend to store state file
 terraform {
   backend "s3" {
-    bucket = "app-terraform-state-bucket"
+    bucket = "terraform_state_bucket"
     key    = "terraform.tfstate"
     region = "aws-region"
   }
