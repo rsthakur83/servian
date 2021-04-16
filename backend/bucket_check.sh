4c509ac1b41168a443eb9e2f24944b8de08363ae
root <root@training-ravinder-4.c.puls-xilium.internal>
2021-04-16 19:29:29 +0000

    added artifacts

--- backend/bucket_check.sh
+++ backend/bucket_check.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 
-bucket1=`aws s3api list-buckets --query "Buckets[].Name" --output text |grep -E app_artifact_bucket  |awk '{print $1}'`
-bucket2=`aws s3api list-buckets --query "Buckets[].Name" --output text |grep -E terraform_state_bucket |awk '{print $2}'`
 
-if [[  $bucket1 = "terraform_state_bucket" || $bucket2 = "app_artifact_bucket" && ( $bucket2 = "terraform_state_bucket" || $bucket1 = "app_artifact_bucket" ) ]];then
+if [[  ( bucket1 = "terraform_state_bucket" && bucket2 = "app_artifact_bucket" ) || ( bucket2 = "terraform_state_bucket" || bucket1 = "app_artifact_bucket" )  ]];then
         echo "Bucket Exist"
 else
         echo "Bucket Doest Not Exist Creating!!!!!"
