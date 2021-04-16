30cb3701fe2db840e536d7dad5b7c6d0f93dbc8a
root <root@training-ravinder-4.c.puls-xilium.internal>
2021-04-16 19:44:35 +0000

    added artifacts

--- backend/bucket_check.sh
+++ backend/bucket_check.sh
@@ -2,7 +2,7 @@
 
 aws s3api list-buckets --query "Buckets[].Name" --output text |grep -E "app_artifact_bucket";aws s3api list-buckets --query "Buckets[].Name" --output text |grep -E "terraform_state_bucket" 
 
-if [ $? -eq 0]; then
+if [ $? -eq 0 ]; then
         echo "Bucket Exist"
 else
         echo "Bucket Doest Not Exist Creating!!!!!"
