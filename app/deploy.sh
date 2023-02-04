4e4929a4ae297874067bd1bc605a8a9ae2c1ffba
rsthakur83 <rsthakur83@yahoo.com>
2023-02-04 13:06:41 +0100

    added change in userdata

--- app/deploy.sh
+++ app/deploy.sh
@@ -10,7 +10,7 @@ if [ "$lc" == "$lcfg1" ];then
 	echo "Creating LC2 Lauch Config and Updating ASG"
 	vpc=`aws ec2 describe-vpcs --filters Name=tag:Name,Values='Servian App VPC' --query 'Vpcs[*].VpcId' --output text`
 	sg=`aws ec2  describe-security-groups --filter Name=vpc-id,Values=$vpc  Name=tag:Name,Values='APP SG' --region aws-region --query 'SecurityGroups[*].[GroupId]' --output text`
-	aws autoscaling create-launch-configuration --launch-configuration-name $lcfg2  --image-id ami-id --instance-type t2.micro --iam-instance-profile  cwdb_iam_profile --security-groups $sg  --user-data file:///root/project/app/deploy/userdata-asg.sh
+	aws autoscaling create-launch-configuration --launch-configuration-name $lcfg2  --image-id ami-id --instance-type t2.micro --iam-instance-profile  cwdb_iam_profile --security-groups $sg  --user-data file:///root/project/servian/app/deploy/userdata-asg.sh
 	aws autoscaling update-auto-scaling-group --auto-scaling-group-name $asg --launch-configuration-name $lcfg2 --min-size 4 --max-size 4
 	sleep 150
 	aws autoscaling update-auto-scaling-group --auto-scaling-group-name $asg --launch-configuration-name $lcfg2 --min-size 2 --max-size 3 --desired-capacity 2
@@ -22,7 +22,7 @@ elif [ "$lc" == "$lcfg2" ];then
 	echo "Creating LC Lauch Config and Updating ASG"
 	vpc=`aws ec2 describe-vpcs --filters Name=tag:Name,Values='Servian App VPC' --query 'Vpcs[*].VpcId' --output text`
 	sg=`aws ec2  describe-security-groups --filter Name=vpc-id,Values=$vpc  Name=tag:Name,Values='APP SG' --region aws-region --query 'SecurityGroups[*].[GroupId]' --output text`	
-	aws autoscaling create-launch-configuration --launch-configuration-name $lcfg1  --image-id ami-id --instance-type t2.micro --iam-instance-profile  cwdb_iam_profile --security-groups $sg  --user-data file:///root/project/app/deploy/userdata-asg.sh
+	aws autoscaling create-launch-configuration --launch-configuration-name $lcfg1  --image-id ami-id --instance-type t2.micro --iam-instance-profile  cwdb_iam_profile --security-groups $sg  --user-data file:///root/project/servian/app/deploy/userdata-asg.sh
 	aws autoscaling update-auto-scaling-group --auto-scaling-group-name $asg --launch-configuration-name $lcfg1 --min-size 4 --max-size 4
 	sleep 150
 	aws autoscaling update-auto-scaling-group --auto-scaling-group-name $asg --launch-configuration-name $lcfg1 --min-size 2 --max-size 3 --desired-capacity 2
